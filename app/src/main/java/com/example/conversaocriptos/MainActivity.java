e222c022e52be697d7b4bd9d8d1190e261115963
Leo Spiering <leonardo.spiering@gmail.com>
2019-03-13 23:20:09 -0300

    done

---
+++ app/src/main/java/com/example/conversaocriptos/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.conversaocriptos;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebClientTask mTask;
+    private TextView mTvBtc;
+    private TextView mTvEth;
+    private TextView mTvLtc;
+    private TextView mTvXrp;
+    private Button mBtn;
+    private ArrayList<Coin> cotacaoes;
+    private  Coin  mBTC;
+    private Coin  mETH;
+    private Coin  mLTC ;
+    private Coin  mXRP;
+    private TextView valor;
+    private Double result;
+    private DecimalFormat precision = new DecimalFormat("0.00000000");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebClient requisicao= new WebClient();
+        startDownload();
+        mTvBtc= findViewById(R.id.tv_value_btc);
+        mTvLtc= findViewById(R.id.tv_value_ltc);
+        mTvXrp= findViewById(R.id.tv_value_xrp);
+        mTvEth= findViewById(R.id.tv_value_eth);
+        valor = findViewById(R.id.valor);
+        mBtn= findViewById(R.id.btn_show);
+
+
+
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Double resultBtc = Double.parseDouble(valor.getText().toString()) / mBTC.getBuy();
+                mTvBtc.setText(precision.format(resultBtc));
+
+                Double resultLtc = Double.parseDouble(valor.getText().toString()) / mLTC.getBuy();
+                mTvLtc.setText(precision.format(resultLtc));
+
+                Double resultXrp = Double.parseDouble(valor.getText().toString()) / mXRP.getBuy();
+                mTvXrp.setText(precision.format(resultXrp));
+
+                Double resultEth = Double.parseDouble(valor.getText().toString()) / mETH.getBuy();
+                mTvEth.setText(precision.format(resultEth));
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+
+            Toast.makeText(this,"Carregando Preços",Toast.LENGTH_LONG).show();
+            startDownload();
+
+
+        }
+        return true;
+    }
+
+
+    public void startDownload() {
+        if (mTask == null || mTask.getStatus() != AsyncTask.Status.RUNNING) {
+            mTask = new WebClientTask();
+            mTask.execute();
+        }
+    }
+
+    class  WebClientTask extends AsyncTask<Void,Void, ArrayList<Coin>>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getApplicationContext(), "Aguarde...", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected ArrayList<Coin> doInBackground(Void... strings) {
+            ArrayList<Coin> coinsList=new ArrayList<>();
+            mBTC = WebClient.getCoin("BTC");
+            mETH = WebClient.getCoin("ETH");
+            mLTC = WebClient.getCoin("LTC");
+            mXRP = WebClient.getCoin("XRP");
+            coinsList.add(mBTC);
+            coinsList.add(mETH);
+            coinsList.add(mLTC);
+            coinsList.add(mXRP);
+            cotacaoes=coinsList;
+            Log.i("BTC",cotacaoes.get(0).getStringBuy());
+            Log.i("ETH",cotacaoes.get(1).getStringBuy());
+            Log.i("XRP",cotacaoes.get(2).getStringBuy());
+            Log.i("LTC",cotacaoes.get(3).getStringBuy());
+            return coinsList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Coin> coins) {
+            super.onPostExecute(coins);
+            //     showProgress(false);
+            if (coins != null) {
+
+                mTvBtc.setText(coins.get(0).getStringBuy());
+                mTvEth.setText(coins.get(1).getStringBuy());
+                mTvLtc.setText(coins.get(2).getStringBuy());
+                mTvXrp.setText(coins.get(3).getStringBuy());
+                Toast.makeText(getApplicationContext(), "Valores Atualizados!", Toast.LENGTH_LONG).show();
+            } else {
+
+                Toast.makeText(getApplicationContext(), "Buscando...", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
+
